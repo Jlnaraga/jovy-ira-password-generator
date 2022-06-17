5d03c14798b6e12f193f423376f82c7959f72a49
Jovy Ira <ira_bonching@yahoo.com>
2022-06-17 18:41:35 -0400

    added functions

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,35 @@ var withnumericCharacter = false;
 var withlowercaseCharacter = false;
 var withuppercaseCharacter = false;
 
+function passwordLength() {
+  passwordLength = prompt("How many characters would you like your password to contain?");
+  console.log(passwordLength);
+}
+
+function specialCharacter() {
+  let text = ("Click OK to confirm including special characters.");
+  withspecialCharacter = confirm(text);
+  console.log(withspecialCharacter);
+}
+
+function numericCharacter() {
+  let text = ("Click OK to confirm including numeric characters.");
+  withnumericCharacter = confirm(text);
+  console.log(withnumericCharacter);
+}
+
+function lowercaseCharacter() {
+  let text = ("Click OK to confirm including lowercase characters.");
+  withlowercaseCharacter = confirm(text);
+  console.log(withlowercaseCharacter);
+}
+
+function uppercaseCharacter() {
+  let text = ("Click OK to confirm including uppercase characters.");
+  withlowercaseCharacter = confirm(text);
+  console.log(withuppercaseCharacter);
+}
+
 function generatePassword() {
   console.log("Hey! You clicked the button!")
 
@@ -15,9 +44,17 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
+  passwordLength()
+  specialCharacter()
+  numericCharacter()
+  lowercaseCharacter()
+  uppercaseCharacter()
+
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
+
 
   passwordText.value = password;
 
